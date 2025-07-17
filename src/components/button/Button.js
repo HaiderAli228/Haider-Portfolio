d5b51537e770d19b9a111938d038bc8c26e5c27f
Haider Ali <144993454+HaiderAli228@users.noreply.github.com>
2025-07-17 10:04:52 +0500

    Update Button.js
    fixing the download button error

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Button.scss";
 
-export default function Button({text, className, href, newTab}) {
+export default function Button({ text, className, href, newTab }) {
+  const handleDownload = () => {
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = href;
+    document.body.appendChild(iframe);
+  };
+
   return (
     <div className={className}>
-      <a className="main-button" href={href} target={newTab && "_blank"}>
+      <a
+        className="main-button"
+        href={newTab ? href : undefined}
+        target={newTab ? "_blank" : undefined}
+        rel="noreferrer"
+        onClick={!newTab ? handleDownload : undefined}
+      >
         {text}
       </a>
     </div>
